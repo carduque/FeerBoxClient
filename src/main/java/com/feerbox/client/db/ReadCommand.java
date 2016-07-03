cdca947c67403f791004567c24a7b9dab2ea075d
cduque <carduque@gmail.com>
2016-07-03 11:36:49 +0200

    Read command asc

--- src/main/java/com/feerbox/client/db/ReadCommand.java
+++ src/main/java/com/feerbox/client/db/ReadCommand.java
@@ -95,7 +95,7 @@ public class ReadCommand extends FeerboxDB {
 			statement.setQueryTimeout(30); // set timeout to 30 sec.
 			// statement.executeUpdate("drop table if exists person");
 			//createCommandsTableIfNotExists(statement);
-			ResultSet rs = statement.executeQuery("select id, time, command, startTime, finishTime, upload, restart, parameter from Commands where upload=0 and startTime is null and finishTime is null order by time desc limit 1");
+			ResultSet rs = statement.executeQuery("select id, time, command, startTime, finishTime, upload, restart, parameter from Commands where upload=0 and startTime is null and finishTime is null order by time asc limit 1");
 			while (rs.next()) {
 				command = new Command();
 				command.setId(rs.getInt("id"));
