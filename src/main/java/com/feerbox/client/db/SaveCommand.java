be673d1d24132c653e27dfb10ca73b0df0c11000
cduque <carduque@gmail.com>
2016-07-03 21:01:02 +0200

    Escape parameters

--- src/main/java/com/feerbox/client/db/SaveCommand.java
+++ src/main/java/com/feerbox/client/db/SaveCommand.java
@@ -27,10 +27,14 @@ public class SaveCommand extends FeerboxDB {
 			if(command.getUpload()){
 				upload=1;
 			}
+			String parameter = null;
+			if(command.getParameter()!=null){
+				parameter = command.getParameter().replaceAll("'","\'");
+			}
 			String sql = "insert into Commands (time, command, serverId, upload, serverCreationTime, restart, parameter) "
 					+ "values(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'localtime'),'" + command.getCommand() + "',  "+command.getServerId()+", "+upload
 							+", STRFTIME('%Y-%m-%d %H:%M:%f', '"+ command.getsetServerCreationTimeFormatted()+"'), "
-									+restart+",'"+command.getParameter()+"')";
+									+restart+",'"+parameter+"')";
 			//logger.debug(sql);
 			statement.executeUpdate(sql);
 			ResultSet rs = statement.executeQuery("SELECT last_insert_rowid() AS rowid FROM Commands LIMIT 1");
