286f32749f48a1c5bdaa2fd282ad490a3cab360e
cduque <carduque@gmail.com>
2017-02-26 13:47:03 +0100

    starting by maven

--- scripts/feerbox-start-mvn.sh
+++ scripts/feerbox-start-mvn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 sudo hwclock -s
-cd /opt/FeerBoxClient/FeerBoxClient/target/classes/
+cd /opt/FeerBoxClient/FeerBoxClient/
 sudo mvn exec:java -Dexec.mainClass="com.feerbox.client.StartFeerBoxClient" &
 disown
