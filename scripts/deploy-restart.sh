96deacd6f6c72eaafd3a445b03eeb1951692e98b
cduque <carduque@gmail.com>
2017-03-12 08:52:38 +0100

    deploy-restart script update

--- scripts/deploy-restart.sh
+++ scripts/deploy-restart.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-cd /opt/FeerBoxClient/FeerBoxClient/scripts
-./feerbox-stop.sh
-cd /opt/FeerBoxClient/FeerBoxClient/
-sudo git fetch origin
-sudo git reset --hard origin/master
-
-sudo mvn clean install -Dmaven.test.skip=true
-cd /opt/FeerBoxClient/FeerBoxClient/scripts
-sudo chmod 777 *.sh
-./feerbox-start.sh
+/bin/bash /opt/FeerBoxClient/FeerBoxClient/scripts/deploy.sh
+sudo reboot
