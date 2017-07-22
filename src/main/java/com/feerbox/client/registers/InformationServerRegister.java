e1e775d2312e0a82a53dece5c480335f4911ede9
cduque <carduque@gmail.com>
2017-07-22 13:20:49 +0200

    disable bulky update

--- src/main/java/com/feerbox/client/registers/InformationServerRegister.java
+++ src/main/java/com/feerbox/client/registers/InformationServerRegister.java
@@ -33,7 +33,7 @@ public class InformationServerRegister extends Thread {
 				uploadAnswers();
 				uploadCleaningService();
 				uploadMACs();
-				uploadBulkyCounterPeople();
+				uploadCounterPeople();
 				uploadCommandsOutput();
 			}
 			//Nothing to update
