13051cd3610f5a9f971623d6cc921ab3b07a4e8c
cduque <carduque@gmail.com>
2017-09-20 23:25:35 +0200

    change parameter script

--- scripts/change-parameter.sh
+++ scripts/change-parameter.sh
@@ -3,3 +3,4 @@ PARAMETER=$1
 VALUE=$2
 
 sudo sed -i 's:^[ \t]*'${PARAMETER}'[ \t]*=\([ \t]*.*\)$:'${PARAMETER}' = '${VALUE}':' /opt/FeerBoxClient/FeerBoxClient/target/classes/config.properties
+cat /opt/FeerBoxClient/FeerBoxClient/target/classes/config.properties
