efae2b81c1dcb6c6e45d6f0bfb2d47f2fee9eb6d
cduque <carduque@gmail.com>
2017-11-23 22:53:22 +0100

    syslog backup script

--- scripts/backup_logs.sh
+++ scripts/backup_logs.sh
@@ -12,7 +12,7 @@ fi
 FILE='/opt/FeerBoxClient/FeerBoxClient/logs'
 now=$(date +"%d_%m_%Y")
 FILE_OUT="${reference}_feerbox-client-logs${now}.tar.gz"
-tar -czvf $FILE_OUT -C $FILE .
-curl --ftp-ssl -k ftp.feerbox.com --user db_backup@happycustomerbox.com:feerboxcompany2015 -T $FILE_OUT
-rm $FILE_OUT
+sudo tar -czvf $FILE_OUT -C $FILE .
+sudo curl --ftp-ssl -k ftp.feerbox.com --user db_backup@happycustomerbox.com:feerboxcompany2015 -T $FILE_OUT
+sudo rm -f $FILE_OUT
 exit 0
--- scripts/backup_syslogs.sh
+++ scripts/backup_syslogs.sh
@@ -22,6 +22,6 @@ FILE10='/var/log/lighttpd/error.log'
 now=$(date +"%d_%m_%Y")
 FILE_OUT="${reference}_feerbox-client-syslogs${now}.tar.gz"
 sudo tar -czvf $FILE_OUT $FILE1 $FILE2 $FILE3 $FILE4 $FILE5 $FILE6 $FILE7 $FILE8 $FILE9 $FILE10
-curl --ftp-ssl -k ftp.feerbox.com --user db_backup@happycustomerbox.com:feerboxcompany2015 -T $FILE_OUT
-rm -f $FILE_OUT
+sudo curl --verbose --ftp-ssl -k ftp.feerbox.com --user db_backup@happycustomerbox.com:feerboxcompany2015 -T $FILE_OUT
+sudo rm -f $FILE_OUT
 exit 0
