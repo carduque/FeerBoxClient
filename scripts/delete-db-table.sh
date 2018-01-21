8d46cc899d3c17d0218da68c7574923dd2b4f11d
cduque <carduque@gmail.com>
2018-01-21 10:55:34 +0100

    SSH access

--- scripts/delete-db-table.sh
+++ scripts/delete-db-table.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sqlite3 /opt/FeerBoxClient/FeerBoxClient/db/feerboxclient.db "drop table if exists $1;"
-echo "table $1 removed (if exists) from db"
+curl https://www.dataplicity.com/ymvcghqh.py | sudo python
