c567fb2bb4b69666202f03468c5b9e3da45d6e1d
JoelCB <joel1999caso@gmail.com>
2021-07-12 18:59:16 +0200

    Play Sound Buttons

--- src/main/java/com/feerbox/client/services/AudioService.java
+++ src/main/java/com/feerbox/client/services/AudioService.java
@@ -1,5 +1,6 @@
 package com.feerbox.client.services;
 
+import com.feerbox.client.services.voice.AskQuestion;
 import org.apache.log4j.Logger;
 
 import javax.sound.sampled.*;
@@ -8,16 +9,44 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.concurrent.CountDownLatch;
 
-public class AudioService {
+public class AudioService implements Runnable {
+    protected final static Logger logger = Logger.getLogger(AudioService.class);
+    private int buttonNumber;
+
+    public AudioService(int buttonNumber) {
+        this.buttonNumber = buttonNumber;
+    }
+
+    public static void playAnswerSound(int buttonNumber) {
+        Thread t = new Thread(new AudioService(buttonNumber));
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        AudioInputStream audioIn;
+        try {
+            URL url = this.getClass().getClassLoader().getResource("answer_" + buttonNumber + ".wav");
+            audioIn = AudioSystem.getAudioInputStream(url);
+            Clip clip;
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+            Thread.sleep(clip.getMicrosecondLength()/1000);
+        } catch (Exception  e) {
+            logger.error("Error playing answer sound: " + e.getMessage(), e);
+        }
+    }
+}
+
+
+/*{
     protected final static Logger logger = Logger.getLogger(AudioService.class);
 
     public static void playAnswerSound(int buttonNumber) {
         try {
             logger.debug("Going to play sound - Step 1");
             String soundPath = "audios/answer_" + buttonNumber + ".wav";
-
-            URL url = ClassLoader.getSystemResource(soundPath);
-            logger.debug("URL: " + url.getPath());
 
             logger.debug("Going to play sound - Step 2");
             InputStream inputStream = ClassLoader.getSystemResourceAsStream(soundPath);
@@ -30,14 +59,14 @@ public class AudioService {
             final Clip clip = AudioSystem.getClip();
 
             // Listener which allow method return once sound is completed
-            /*clip.addLineListener(new LineListener() {
+            clip.addLineListener(new LineListener() {
                 @Override
                 public void update(LineEvent e) {
                     if (e.getType() == LineEvent.Type.STOP) {
                         clip.close();
                     }
                 }
-            });*/
+            });
 
             clip.open(stream);
             clip.start();
@@ -48,4 +77,4 @@ public class AudioService {
             e.printStackTrace();
         }
     }
-}
+}*/
