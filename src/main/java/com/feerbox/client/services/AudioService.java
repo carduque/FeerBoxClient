aa17dfd944705a0a1924b9dbe0f7d346c7898390
JoelCB <joel1999caso@gmail.com>
2021-07-12 17:26:27 +0200

    Play Sound Buttons

--- src/main/java/com/feerbox/client/services/AudioService.java
+++ src/main/java/com/feerbox/client/services/AudioService.java
@@ -13,36 +13,34 @@ public class AudioService {
         try {
             final CountDownLatch syncLatch = new CountDownLatch(1);
 
-            try {
-                String soundPath = "audios/answer_" + buttonNumber + ".wav";
-                AudioInputStream stream = AudioSystem.getAudioInputStream(ClassLoader.getSystemResourceAsStream(soundPath));
+            logger.debug("Going to play sound 1");
+            String soundPath = "audios/answer_" + buttonNumber + ".wav";
 
-                logger.debug("Going to play sound");
-                final Clip clip = AudioSystem.getClip();
+            logger.debug("Going to play sound 2");
+            AudioInputStream stream = AudioSystem.getAudioInputStream(ClassLoader.getSystemResourceAsStream(soundPath));
 
-                // Listener which allow method return once sound is completed
-                clip.addLineListener(new LineListener() {
-                    @Override
-                    public void update(LineEvent e) {
-                        if (e.getType() == LineEvent.Type.STOP) {
-                            clip.close();
-                            syncLatch.countDown();
-                        }
+            logger.debug("Going to play sound 3");
+            final Clip clip = AudioSystem.getClip();
+
+            // Listener which allow method return once sound is completed
+            clip.addLineListener(new LineListener() {
+                @Override
+                public void update(LineEvent e) {
+                    if (e.getType() == LineEvent.Type.STOP) {
+                        clip.close();
+                        syncLatch.countDown();
                     }
-                });
+                }
+            });
 
-                clip.open(stream);
-                clip.start();
-                logger.debug("Playing answer " + buttonNumber + " sound (" + soundPath + ")");
-            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException  e) {
-                syncLatch.countDown();
-                logger.debug("Error playing answer sound: " + e.getMessage());
-                e.printStackTrace();
-            }
+            clip.open(stream);
+            clip.start();
 
+            logger.debug("Playing answer " + buttonNumber + " sound (" + soundPath + ")");
             syncLatch.await();
-        }  catch (InterruptedException e) {
-            logger.error("Error sound sync" + e.getMessage());
+        } catch (InterruptedException | UnsupportedAudioFileException | IOException | LineUnavailableException  e) {
+            logger.debug("Error playing answer sound: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
