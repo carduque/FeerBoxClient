95ea26cc00287de8c2143bbe1ca3d2e3dc0f1abf
JoelCB <joel1999caso@gmail.com>
2021-07-12 18:18:24 +0200

    Play Sound Buttons

--- src/main/java/com/feerbox/client/services/AudioService.java
+++ src/main/java/com/feerbox/client/services/AudioService.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import javax.sound.sampled.*;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.CountDownLatch;
 
 public class AudioService {
@@ -13,13 +14,16 @@ public class AudioService {
         try {
             final CountDownLatch syncLatch = new CountDownLatch(1);
 
-            logger.debug("Going to play sound 1");
+            logger.debug("Going to play sound - Step 1");
             String soundPath = "audios/answer_" + buttonNumber + ".wav";
 
-            logger.debug("Going to play sound 2");
-            AudioInputStream stream = AudioSystem.getAudioInputStream(ClassLoader.getSystemResourceAsStream(soundPath));
+            logger.debug("Going to play sound - Step 2");
+            InputStream inputStream = ClassLoader.getSystemResourceAsStream(soundPath);
 
-            logger.debug("Going to play sound 3");
+            logger.debug("Going to play sound - Step 3");
+            AudioInputStream stream = AudioSystem.getAudioInputStream(inputStream);
+
+            logger.debug("Going to play sound - Step 4");
             final Clip clip = AudioSystem.getClip();
 
             // Listener which allow method return once sound is completed
