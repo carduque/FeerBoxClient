4f8ac482adf31b36823fc77ff5b0e91c10160b4d
JoelCB <joel1999caso@gmail.com>
2021-07-28 18:18:17 +0200

    Fix PIR Audio

--- src/main/java/com/feerbox/client/registers/PresenceDetectorRegister.java
+++ src/main/java/com/feerbox/client/registers/PresenceDetectorRegister.java
@@ -13,6 +13,7 @@ import org.apache.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -42,7 +43,8 @@ public class PresenceDetectorRegister extends Thread {
 		        if(event.getState().isHigh()){
 		        	if(lastPIRDetection != 0 && ((new Date().getTime() - lastPIRDetection) > pauseBetween)) {
 						logger.debug("Presence detected");
-						AudioService.playSound("welcome");
+						URL soundUrl = AudioService.getAudioUrl("welcome");
+						AudioService.playSound(soundUrl);
 		        	}
 		        	lastPIRDetection = new Date().getTime();
 		        }
